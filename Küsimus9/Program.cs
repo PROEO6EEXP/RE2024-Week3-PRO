1c24640efe88c8689577b151e211e16bd0be4019
PROEO6EEXP <ekke.jakob.mesila@tktk.ee>
2024-09-30 19:34:42 +0300

    add week3 code

---
+++ Küsimus9/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+Console.WriteLine("Enter your grade:");
+
+char userGrade = Char.Parse(Console.ReadLine());
+
+//if (userGrade == 'A')
+//{
+//    Console.WriteLine("Suurepärane!");
+//}
+//else if (userGrade == 'B')
+//{
+//    Console.WriteLine("Väga hea!");
+//}
+//else if (userGrade == 'C')
+//{
+//    Console.WriteLine("Hea!");
+//}
+//else if (userGrade == 'D')
+//{
+//    Console.WriteLine("Rahuldav");
+//}
+//else if (userGrade == 'E')
+//{
+//    Console.WriteLine("Kasin");
+//}
+//else if (userGrade == 'F')
+//{
+//    Console.WriteLine("Puudulik.");
+//}
+//else
+//{
+//    Console.WriteLine("Vale väärtus.");
+//}
+
+switch (userGrade)  //väärtus mida kontrollitakse
+{
+    case ('A'):  
+        Console.WriteLine("Suurepärane!");
+        break;
+    case ('B'):
+        Console.WriteLine("Väga hea!");
+        break;
+    case ('C'):
+        Console.WriteLine("Hea!");
+        break;
+    case ('D'):
+        Console.WriteLine("Rahuldav");
+        break;
+    case ('E'):
+        Console.WriteLine("Kasin");
+        break;
+    case ('F'):
+        Console.WriteLine("Puudulik.");
+        break;
+    default:
+        Console.WriteLine("Vale väärtus.");
+        break;
+
+}
+
+
---
+++ Week3ifAndSwitch/Program.cs
@@ -0,0 +1,8 @@
+﻿//Modulus - %
+
+int a = 7;
+int b = 29;
+
+int c = a % b; //+/-/* - /
+
+Console.WriteLine(c);
